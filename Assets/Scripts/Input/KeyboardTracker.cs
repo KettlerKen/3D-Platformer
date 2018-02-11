68b599fb9958367f6d863f8adea2abc295721dbc
Kenny <kettlken@gmail.com>
2018-02-10 21:59:40 -0800

    Added Main Files

---
+++ Assets/Scripts/Input/KeyboardTracker.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyboardTracker : DeviceTracker {
+
+	public AxisKeys[] axisKeys;
+	public KeyCode[] buttonKeys;
+
+	//Overide built in Unity: resests length of two input arrays to match input manager
+	void Reset() {
+		im = GetComponent<InputManager> ();
+		axisKeys = new AxisKeys[im.axisCount];
+		buttonKeys = new KeyCode[im.buttonCount];
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		//POPULATE INPUT DATA STRUCTURE TO PASS TO INPUT MANAGER
+
+	 	//Input found
+		for (int i = 0; i < axisKeys.Length; i++) {
+			float val = 0f;
+			if (Input.GetKey(axisKeys [i].positive)) {
+				val = val + 1f;
+				newData = true;
+			}
+			if (Input.GetKey(axisKeys [i].negative)) {
+				val = val - 1f;
+				newData = true;
+			}
+			data.axes [i] = val;
+		}
+
+		for (int i = 0; i < buttonKeys.Length; i++) {
+			if (Input.GetKey(buttonKeys [i])) {
+				data.buttons [i] = true;
+				newData = true;
+			}
+		}
+
+		//CHECK FOR INPUT
+		if (newData) {
+			im.PassInput (data);
+			newData = false;
+			data.Reset();
+		}
+	}
+}
+
+[System.Serializable]
+public struct AxisKeys {
+	public KeyCode positive;
+	public KeyCode negative;
+}
---
+++ Assets/Scripts/Model/PlayerHitbox.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider))]
+public class PlayerHitbox : MonoBehaviour {
+	BoxCollider col;
+
+	//on awake add this event to an events list
+	void Awake(){
+		col = GetComponent<BoxCollider> ();
+	}
+
+}
